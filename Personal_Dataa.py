39ca63715e9550cbbec6f56532e5b606218e02bc
Kimberly <ae.xerx@gmail.com>
2024-03-23 21:55:02 +0800

    Def function

--- Personal_Dataa.py
+++ Personal_Dataa.py
@@ -5,7 +5,8 @@ from time import sleep
 
 
 # Define a function to create fancy text using pyfiglet
-
+def fancy_text(text):
+    return '\033[94;1m' + pyfiglet.figlet_format(text, font='isometric1') +  '\033[=0m'
 
 # Get user input for name
 name = input("Enter your name: ")
