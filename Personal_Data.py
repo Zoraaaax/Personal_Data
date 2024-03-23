08b943cde796a025877970ebcd2c3c5604970526
Kimberly <ae.xerx@gmail.com>
2024-03-23 20:48:41 +0800

    Prompt the user to input their information

--- Personal_Data.py
+++ Personal_Data.py
@@ -1,5 +1,10 @@
 # Get user input for name
+name = input("Enter your name: ")
 # Get user input for dream job
+dream_job = input("Enter your dream job: ")
 # Get user input for civil status
+civil_status = input("Enter your civil status: ")
 # Get user input for address
+address = input("Enter your address")
 # Get user input for contact number
+contact_number = input("Enter your contact number: ")
