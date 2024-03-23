93b282bba69a5c9638e795567be97b101c47f96c
Kimberly <ae.xerx@gmail.com>
2024-03-23 21:43:44 +0800

    Pseudocode

--- Personal_Dataa.py
+++ Personal_Dataa.py
@@ -1,2 +1,7 @@
 # Import the pyfiglet
 # Import the sleep function from the time module
+# Get user input for name
+# Get user input for dream job
+# Get user input for civil status
+# Get user input for address
+# Get user input for contact number
