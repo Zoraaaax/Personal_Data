1be16cf482dfc4328b94e070bbcf5aaff5429c96
Kimberly <ae.xerx@gmail.com>
2024-03-23 21:53:02 +0800

    Spaces

--- Personal_Dataa.py
+++ Personal_Dataa.py
@@ -3,6 +3,10 @@ import pyfiglet
 # Import the sleep function from the time module
 from time import sleep
 
+
+
+
+
 # Get user input for name
 name = input("Enter your name: ")
 # Get user input for dream job
