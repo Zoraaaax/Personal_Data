fa05c1022dd11896a09f39e4462e4ddea8eda527
Kimberly <ae.xerx@gmail.com>
2024-03-23 22:06:57 +0800

    Looping

--- Personal_Dataa.py
+++ Personal_Dataa.py
@@ -2,13 +2,9 @@
 import pyfiglet
 # Import the sleep function from the time module
 from time import sleep
-
-
 # Define a function to create fancy text using pyfiglet
 def fancy_text(text):
     return '\033[94;1m' + pyfiglet.figlet_format(text, font='isometric1') + '\033[=0m'
-
-
 # Get user input for name
 name = input("Enter your name: ")
 # Get user input for dream job
@@ -24,3 +20,6 @@ output = ["My name is " + name + ", and my dream job is " + dream_job + "."
 # Iterate through each line in the output
 for line in output:
     fancy_line = fancy_text(line)
+    for c in fancy_line:
+        print(c, end='', flush=True)
+        sleep(0.001)
