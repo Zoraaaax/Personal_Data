c26badcabb1561459c96aca2abdf2d71d4876268
Kimberly Aguilar <138700691+Zoraaaax@users.noreply.github.com>
2024-03-23 22:18:04 +0800

    Fancy_Personal_Data.py

--- Fancy_Personal_Data.py
+++ Fancy_Personal_Data.py
@@ -3,8 +3,12 @@ import pyfiglet
 # Import the sleep function from the time module
 from time import sleep
 # Define a function to create fancy text using pyfiglet
+
+
 def fancy_text(text):
     return '\033[94;1m' + pyfiglet.figlet_format(text, font='isometric1') + '\033[=0m'
+
+    
 # Get user input for name
 name = input("Enter your name: ")
 # Get user input for dream job
@@ -13,6 +17,7 @@ dream_job = input("Enter your dream job: ")
 address = input("Enter your address: ")
 # Get user input for contact number
 contact_number = input("Enter your contact number: ")
+
 # Create an output string with formatted text
 output = ["My name is " + name + ", and my dream job is " + dream_job + "."
           + "I live in " + address + ", and my contact number is" + contact_number]
